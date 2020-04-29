965410b9f9cbc5c8aba00cf4f6fe3c4dae40752f
nilesdsjr <niles.deem@gmail.com>
2020-04-28 21:58:28 -0300

    stilo python yapf

--- src/controller.py
+++ src/controller.py
@@ -10,29 +10,33 @@ def main(parser):
     model = Model()
     model.run(parser)
 
+
 if __name__ == '__main__':
 
     _log_stream = LogStream()
     log = _log_stream.log_stream(origin=__name__)
     parser = argparse.ArgumentParser(
-        description='JobXplainer - Diagnóstico de performance para scripts Hive SQL.')
+        description=
+        'JobXplainer - Diagnóstico de performance para scripts Hive SQL.')
     parser.add_argument(
         '-f',
         '--file',
         dest='sql_file',
         default='xplain_this.sql',
-        help='Aceita caminho para um arquivo SQL (default: /path/to/file/xplain_this.sql)')
-    parser.add_argument(
-        '-d',
-        '--directory',
-        dest='sql_dir',
-        default='sql_scripts/',
-        help='(default: /path/to/directory/sql_scripts/)')
+        help=
+        'Aceita caminho para um arquivo SQL (default: /path/to/file/xplain_this.sql)'
+    )
+    parser.add_argument('-d',
+                        '--directory',
+                        dest='sql_dir',
+                        default='sql_scripts/',
+                        help='(default: /path/to/directory/sql_scripts/)')
 
     if not len(sys.argv) > 1:
         log.info((parser.parse_args()).sql_file)
         log.info(
-            'Controller - Você não acionou nenhum parâmetro. Execute --help para saber opções. Execução padrão iniciada.')
+            'Controller - Você não acionou nenhum parâmetro. Execute --help para saber opções. Execução padrão iniciada.'
+        )
         main(parser)
         log.info('Controller - mensagem final')
     else:
