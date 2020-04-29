a1b24aa359d5bb698d5f931afb8aac4cbc8d8892
nilesdsjr <niles.deem@gmail.com>
2020-04-28 21:59:54 -0300

    stilo python, criação da classe Configuration que carrega o yaml

--- src/settings.py
+++ src/settings.py
@@ -1,13 +1,19 @@
 import os
 import sys
 import logging
+import yaml
 from datetime import datetime
 
 
 class Settings:
 
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    CONFIG_PATH = os.path.join(ROOT_DIR, 'configuration.conf')
+    CONFIG_PATH = os.path.join(
+        os.path.abspath(os.path.join(ROOT_DIR, os.pardir)), 'resources')
+
+    def set_kinit(self):
+
+        pass
 
 
 class LogStream:
@@ -18,18 +24,67 @@ class LogStream:
 
         root = logging.getLogger(origin)
         root.setLevel(logging.DEBUG)
-        if to_file:
 
-            log_dir = os.path.join(os.path.abspath(os.path.join(ROOT_DIR, os.pardir)), 'LOGS')
-            print(log_dir)
+        if to_file:
+            log_dir = os.path.join(
+                os.path.abspath(os.path.join(ROOT_DIR, os.pardir)), 'LOGS')
+
             if not os.path.isdir(log_dir):
                 os.makedirs(log_dir)
-            handler = logging.FileHandler(os.path.join(log_dir, origin +'_jobXplainer_' +((datetime.now()).strftime('%Y_%m_%d_%Hh%Mm%Ss')) + '.log'))
+
+            handler = logging.FileHandler(
+                os.path.join(
+                    log_dir, origin + '_jobXplainer_' +
+                    ((datetime.now()).strftime('%Y_%m_%d_%Hh%Mm%Ss')) +
+                    '.log'))
+
         else:
             handler = logging.StreamHandler(sys.stdout)
+
         handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         root.addHandler(handler)
-
+        root.info('Logging Handler para ' + origin + ' adicionado!')
         return root
+
+
+class Configuration:
+    def load_config(self, path_config):
+
+        _logstream = LogStream()
+        log = _logstream.log_stream(origin=__name__)
+        dir_ls = list(os.scandir(path_config))
+        dir_ls.sort(key=os.path.getctime, reverse=True)
+
+        _path_config_file = [
+            file.name for file in dir_ls if file.name == 'jobXplainer.yaml'
+        ]
+
+        if len(_path_config_file) > 1:
+            log.error(
+                'Mais de um arquivo jobXplainer.yaml foi identificado. \n Confira os arquivos em '
+                + path_config)
+            exit()
+
+        elif len(_path_config_file) < 1:
+            log.error(
+                'Nenhum arquivo de configuração jobXplainer.yaml encontrado.\n Confira em '
+                + path_config)
+            exit()
+
+        log.info('Carregando arquivo de configuration.')
+        path_config_file = os.path.join(path_config, _path_config_file[0])
+
+        try:
+            with open(path_config_file, 'rb') as yml:
+                config = yaml.safe_load(yml)
+        except Exception as e:
+
+            log.error('Carregamento de arquivo yaml falhou. Confira em ' +
+                      path_config)
+            raise (e)
+
+        log.info('Arquivo de configuration carregado.')
+        return config
