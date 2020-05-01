11b1f785049640f031d984466e2da2ec1f631801
nilesdsjr <niles.deem@gmail.com>
2020-05-01 19:54:45 -0300

    criação de classe CliController para input cmd

--- src/controller.py
+++ src/controller.py
@@ -1,14 +1,105 @@
+import os
 import sys
 import argparse
 import logging
 from model import Model
-from settings import LogStream
+from settings import Settings, LogStream, Configuration
+
+
+class CliController:
+
+    '''Can only be called with a argparse Object.'''
+
+    def __init__(self, parser):
+
+        
+        self.settings = Settings()
+        _log_stream = LogStream()
+        self.log = _log_stream.log_stream(origin=__class__.__name__)
+        self.parser = parser
+        p = self.parser.parse_args()       
+        self.all_defaults = {key: parser.get_default(key) for key in vars(p)}
+        print(self.all_defaults)
+
+
+    def cli_options(self):
+
+        self.p = self.parser.parse_args()
+        default_f = self.all_defaults['sql_file']
+        print(default_f)
+        default_d = self.all_defaults['sql_dir']
+        print(default_d)
+        default_sql_file = os.path.join(self.settings.ROOT_DIR, default_f )
+        default_sql_dir = os.path.join(self.settings.ROOT_DIR, default_d )
+
+        if self.p.sql_file == 'xplain_this.sql' and self.p.sql_dir == 'sql_scripts/':
+            
+            self.sql_file = default_sql_file
+            self.sql_dir = default_sql_dir
+            self.log.warn('Nenhum parâmetro recebido. Execução default carregada.')
+            self.sql_exec = 'Default'
+
+        elif self.p.sql_file != 'xplain_this.sql' and self.p.sql_dir == 'sql_scripts/':
+
+            self.sql_file = self.p.sql_file
+            self.sql_dir = default_sql_dir
+            self.log.info('Caminho para arquivo SQL carregado.')
+            self.sql_exec = 'file'
+        
+        elif self.p.sql_file == 'xplain_this.sql' and self.p.sql_dir != 'sql_scripts/':
+
+            self.sql_file = default_sql_file
+            self.sql_dir = self.p.sql_dir
+            self.log.info('Caminho para diretório SQL carregado.')
+            self.sql_exec = 'dir'
+
+        cli_options = {
+            'sql_metadata' : {
+                'path_file' : self.sql_file,
+                'path_dir' : self.sql_dir,
+                'exec' : self.sql_exec
+            },
+        }
+
+        return cli_options
 
 
 def main(parser):
 
-    model = Model()
-    model.run(parser)
+    settings = Settings()
+    _config = Configuration()
+    config = _config.load_config(path_config=settings.CONFIG_PATH)
+    p = parser.parse_args()
+
+    specify_log_path = config['profile']['logging']['specify_log_path']
+    path_to_log_dir = config['profile']['logging']['path_to_dir']
+
+    _root_log = LogStream()
+
+    if p.log_dir == 'LOG_DIR' and not specify_log_path:
+
+        print(1)
+        log = _root_log.root_logger(silent_logger=p.silent_logger)
+
+    elif p.log_dir == 'LOG_DIR' and specify_log_path:
+
+        print(2)
+        log = _root_log.root_logger(silent_logger=p.silent_logger, _log_dir=path_to_log_dir)
+        
+    elif p.log_dir != 'LOG_DIR' and not specify_log_path:
+        print(3)    
+        log = _root_log.root_logger(silent_logger=p.silent_logger, _log_dir=p.log_dir)
+
+
+    cliController = CliController(parser)
+
+    cli_options = cliController.cli_options()
+
+
+    if not len(sys.argv) > 1: log.info('Você não acionou nenhum parâmetro. Execute --help para saber opções. Execução padrão iniciada.')
+
+    model = Model(cli_options)
+    model.run()
 
 if __name__ == '__main__':
 
@@ -40,9 +131,9 @@ if __name__ == '__main__':
         '-s',
         '--silent',
         dest='silent_logger',
-        default=False,
+        default='yes',
         help=
-        'Visualizar também em linha de comando ou log silencioso para arquivo. True ou False. (default: False)'
+        'Log silencioso para arquivo ou também exibir na linha de comando. yes ou no. (default: yes)'
     )
 
     parser.add_argument(
@@ -53,24 +144,4 @@ if __name__ == '__main__':
         'Pasta destino dos arquivos de LOG. (default: ./LOGS/)'
     )    
 
-    p =parser.parse_args()
-    _log_stream = LogStream()
-
-    if p.log_dir == 'ROOT_DIR':
-
-        root_log = _log_stream.root_logger(silent_logger=p.silent_logger)
-
-    else:
-
-        root_log = _log_stream.root_logger(silent_logger=p.silent_logger, _log_dir=p.log_dir)
-
-    if not len(sys.argv) > 1:
-        root_log.info((parser.parse_args()).sql_file)
-        root_log.info(
-            'Controller - Você não acionou nenhum parâmetro. Execute --help para saber opções. Execução padrão iniciada.'
-        )
-        main(parser)
-        root_log.info('Controller - mensagem final')
-    else:
-        root_log.info((parser.parse_args()).sql_dir)
-        main(parser)
+    main(parser)
