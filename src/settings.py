33c0ff8d0de0e1ceb61c6fe294ae8a0888edc087
nilesdsjr <niles.deem@gmail.com>
2020-05-01 20:01:27 -0300

    log para stdout testado com flag, classe security criada

--- src/settings.py
+++ src/settings.py
@@ -2,62 +2,112 @@ import os
 import sys
 import logging
 import yaml
+import getpass
 from datetime import datetime
 
 
 class Settings:
+
+
     def __init__(self, root_file=__file__):
 
         self.ROOT_DIR = os.path.dirname(os.path.abspath(root_file))
         self.CONFIG_PATH = os.path.join(
             os.path.abspath(os.path.join(self.ROOT_DIR, os.pardir)),
             'resources')
-
-    def set_kinit(self):
-
-        pass
+        self.LOG_DIR = os.path.join(
+            os.path.abspath(os.path.join(self.ROOT_DIR, os.pardir)), 'LOGS')
 
 
 class LogStream:
 
     settings = Settings()
 
-    def log_stream(self, origin, to_file=True, ROOT_DIR=settings.ROOT_DIR):
+    def __init__(self, specify_log_path=False, settings=settings):
 
-        root = logging.getLogger(origin)
-        root.setLevel(logging.DEBUG)
+        self.specify_log_path = specify_log_path
+        self.log_dir = settings.LOG_DIR
+
+    def log_stream(self, origin, to_file=True, _log_dir=settings.LOG_DIR):
+
+        log = logging.getLogger(origin)
+        log.setLevel(logging.DEBUG)
 
         if to_file:
-            log_dir = os.path.join(
-                os.path.abspath(os.path.join(ROOT_DIR, os.pardir)), 'LOGS')
 
-            if not os.path.isdir(log_dir):
-                os.makedirs(log_dir)
+            if not os.path.isdir(_log_dir):
+
+                os.makedirs(_log_dir)
 
             handler = logging.FileHandler(
                 os.path.join(
-                    log_dir, origin + '_jobXplainer_' +
+                    _log_dir, origin + '_jobXplainer_' +
                     ((datetime.now()).strftime('%Y_%m_%d_%Hh%Mm%Ss')) +
                     '.log'))
 
         else:
+
             handler = logging.StreamHandler(sys.stdout)
 
         handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
-        root.addHandler(handler)
-        root.info('Logging Handler para ' + origin + ' adicionado!')
+        log.addHandler(handler)
+        log.info('Logging Handler para {} adicionado!'.format(origin))
 
-        return root
+        return log
+
+    def root_logger(self, silent_logger='yes', _log_dir=settings.LOG_DIR):
+
+
+        if not os.path.isdir(_log_dir):
+
+            os.makedirs(_log_dir)
+
+        if silent_logger == 'no': 
+            print(silent_logger)
+            root = logging.getLogger()
+            root.setLevel(logging.DEBUG)
+            handler = logging.StreamHandler(sys.stdout)
+            handler.setLevel(logging.DEBUG)
+            formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            root.addHandler(handler)
+            root.info('Logging Handler para stdout_jobXplainer adicionado!')
+
+        elif silent_logger == 'yes':
+        
+            root = logging.getLogger()
+            root.setLevel(logging.DEBUG)
+    
+            handler = logging.FileHandler(
+                os.path.join(
+                    _log_dir, 'All_jobXplainer_' +
+                    ((datetime.now()).strftime('%Y_%m_%d_%Hh%Mm%Ss')) + '.log'))
+    
+            handler.setLevel(logging.DEBUG)
+            formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            root.addHandler(handler)
+            root.info('Logging Handler para All_jobXplainer adicionado!')
+            
+            return root
+
+        elif silent_logger not in ('no', 'yes'):
+            
+            print('Valor {} silent logger não reconhecido'.format(silent_logger))
 
 
 class Configuration:
+
+
     def load_config(self, path_config):
 
         _logstream = LogStream()
-        log = _logstream.log_stream(origin=__name__)
+        log = _logstream.log_stream(origin=__class__.__name__)
         dir_ls = list(os.scandir(path_config))
         dir_ls.sort(key=os.path.getctime, reverse=True)
 
@@ -66,15 +116,19 @@ class Configuration:
         ]
 
         if len(_path_config_file) > 1:
+
             log.error(
                 'Mais de um arquivo jobXplainer.yaml foi identificado. \n Confira os arquivos em '
                 + path_config)
+
             exit()
 
         elif len(_path_config_file) < 1:
+
             log.error(
                 'Nenhum arquivo de configuração jobXplainer.yaml encontrado.\n Confira em '
                 + path_config)
+
             exit()
 
         log.info('Carregando arquivo de configuration.')
@@ -97,3 +151,21 @@ class Configuration:
         log.info('Arquivo de configuration carregado.')
 
         return config
+
+
+class Security:
+    def __init__(self):
+
+        settings = Settings()
+        configuration = Configuration()
+        config = configuration.load_config(settings.CONFIG_PATH)
+        ktb_path = config['profile']['kerberos']['keytab']
+        self.ktb_path = ktb_path.format(username=getpass.getuser())
+
+    def kbrs_auth(self):
+
+        pass
+
+    def set_kinit(self, ktb_path):
+
+        pass
