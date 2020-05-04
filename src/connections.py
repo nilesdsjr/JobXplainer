0f0e3965cdfbf6a72c7c96d79faf3e1162f9a8bc
nilesdsjr <niles.deem@gmail.com>
2020-05-03 23:23:33 -0300

    tratamento de tentativas de conn no init, singleton conn em init

--- src/connections.py
+++ src/connections.py
@@ -1,29 +1,71 @@
 import jaydebeapi
+import os
 from singleton import Singleton
+from settings import Settings, Configuration
 
 
-@Singleton
-class HiveClient(object):
-    def __init__(self, config):
+class HiveClient(object, metaclass=Singleton):
+    
 
-        self.database = config['profile']['hive']['jdbc']['database']
-        self.driver = config['profile']['hive']['jdbc']['driver']
-        self.server = config['profile']['hive']['jdbc']['server']
-        self.principal = config['profile']['hive']['jdbc']['principal']
-        self.port = config['profile']['hive']['jdbc']['port']
+    def __init__(self):
 
-        # JDBC connection string
-        self.url = ("jdbc:hive2://" + self.server + ":" + str(self.port) +
-                    "/" + self.database + ";principal=" + self.principal + ";")
 
-    #Connect to HiveServer2
-    def get_conn(self):
+        settings = Settings()
+        _config = Configuration()
+        config = _config.load_config(path_config=settings.CONFIG_PATH)
+        _path_dependencies = config['profile']['hive']['jdbc']['dependencies']
+        _database = config['profile']['hive']['jdbc']['database']
+        _driver = config['profile']['hive']['jdbc']['driver']
+        _server = config['profile']['hive']['jdbc']['server']
+        _principal = config['profile']['hive']['jdbc']['principal']
+        _port = config['profile']['hive']['jdbc']['port']
+        _url = ('jdbc:hive2://{}:{}/{};principal={};'.format(_server, str(_port), _database, _principal))
+        path_dependencies = os.path.join(settings.CONFIG_PATH, 'dependencies/hive-jdbc-3.1.2-standalone.jar')
 
-        self.conn = jaydebeapi.connect("org.apache.hive.jdbc.HiveDriver",
-                                       self.url)
-        self.cursor = self.conn.cursor()
+        if _path_dependencies: 
 
-        return self.cursor
+            try:
+            
+                _conn = jaydebeapi.connect(jclassname='org.apache.hive.jdbc.HiveDriver', url=_url, jars=_path_dependencies)
+
+            except Exception as e:
+               
+                raise e
+
+        elif os.path.isfile(path_dependencies):
+            
+            try:
+
+                _conn = jaydebeapi.connect(jclassname='org.apache.hive.jdbc.HiveDriver', url=_url, jars=path_dependencies)
+            
+            except Exception as e:
+                    
+                raise e
+
+        else:
+            
+            try:
+                
+                _conn = jaydebeapi.connect(jclassname='org.apache.hive.jdbc.HiveDriver', url=_url)
+
+            except Exception as e:
+                    
+                raise e
+
+        self.cursor = _conn.cursor()
+
+
+    def execute(self, sql):
+
+
+        self.cursor.execute(sql)
+        results = self.cursor.fetchall()
+
+        return results
+
 
     def close(self):
-        self.conn.close()
+
+
+        return self.cursor.close()
+
