f63c598bd4a32ecef9938b879a3a4a3730a39544
nilesdsjr <niles.deem@gmail.com>
2020-05-03 23:21:38 -0300

    tipificação dos exceptions, criação da run

--- src/model.py
+++ src/model.py
@@ -3,8 +3,9 @@ import os
 import re
 from datetime import datetime
 from settings import LogStream
+from connections import HiveClient
 
-class Model:
+class ModelJobxplainer:
 
 
     def __init__(self, options):
@@ -26,9 +27,11 @@ class Model:
 
             else:
                 self.log.error('Extensão do arquivo {} não reconhecida. Apenas .sql ou .SQL são aceitos.'.format(path_file))
+                raise AttributeError('Extensão do arquivo {} não reconhecida. Apenas .sql ou .SQL são aceitos.'.format(path_file))
 
         else:
-            self.log.error('Dado {} não é um arquivo.'.format(path_file))
+            self.log.error('{} não existe. Este arquivo é default quando nenhum outro arquivo ou diretório é passado. Crie o arquivo com query ou indique o caminho para um arquivo ou diretório com arquivos sql.'.format(path_file))
+            raise AttributeError('{} não existe.'.format(path_file))
 
 
     def get_sql_dir(self):
@@ -41,9 +44,8 @@ class Model:
 
             if not os.listdir(path_dir):
 
-                self.log.error(
-                    "Diretório para arquivos sql está vazio. Indique o diretório correto e reinicie o jobXplainer."
-                )
+                self.log.error("Diretório para arquivos sql está vazio. Indique o diretório correto e reinicie o jobXplainer.")
+                raise AttributeError('Diretório para arquivos sql está vazio.')
 
             else:
                 _dir_ls = os.scandir(path_dir)
@@ -51,12 +53,16 @@ class Model:
                 dir_ls.sort(key=os.path.getctime, reverse=True)
                 path_sql_files = [file for file in dir_ls if re.search(".sql$", file, re.IGNORECASE)]
 
+                if len(path_sql_files) < 1:
+
+                    self.log.error("Diretório não possui arquivos sql. Indique o diretório correto e reinicie o jobXplainer.")
+                    raise AttributeError('Diretório não possui arquivos sql.')                
+
                 return path_sql_files
 
         else:
-            self.log.error(
-                "Diretório para arquivos sql não existe. Indique o diretório correto e reinicie o jobXplainer."
-            )
+            self.log.error("Diretório para arquivos sql não existe. Indique o diretório correto e reinicie o jobXplainer.")
+            raise AttributeError('Diretório para arquivos sql não existe.')
 
 
     def get_query(self, sql_file):
@@ -68,31 +74,83 @@ class Model:
                 sql = f.read()
             f.close()
 
-        except Exception as e:
+        except IOError('Erro crítico ao tentar abrir arquivo sql.') as io:
 
-            self.log.error('Erro crítico ao tentar abrir arquivo sql.',
-                           exec_info=True)
-            raise (e)
+            self.log.error('Erro crítico ao tentar abrir arquivo sql.', exec_info=True)
+            raise io
 
         return sql
 
-    def get_explain(self):
-
-        pass
 
     def save_response(self):
 
+
         pass
 
 
-    def run_query(self, sql, cursor):
+    def get_explain(self, sql):
+        
 
-        cursor.execute(sql)
-        results = cursor.fetchall()
+        hive = HiveClient()
+        result = hive.execute(sql)
 
-        return results
+
+        return result
+
+    def _exec_file(self):
+
+
+        sql_file = self.get_sql_file()
+        sql = self.get_query(sql_file)
+        _xplain = self.get_explain(sql)
+
+        return _xplain
+
+    
+    def _exec_dir(self):
+
+
+        sql_dir = self.get_sql_dir()
+        sqls = {sql_file:self.get_query(sql_file) for sql_file in sql_dir}
+        _xplain = [self.get_explain(sql) for sql in sqls]
+
+        return _xplain
 
 
     def run(self):
+        
 
-        pass
+        sql_exe_type = self.opt['sql_metadata']['exec_type']
+        
+
+        if sql_exe_type == 'default':
+
+            try:
+
+                xplain = self._exec_file()
+                print(xplain)
+
+            except:
+
+                self.log.warning('Nenhum arquivo xplain_this.sql encontrado. Tentando diretório sql_scripts/.')
+
+            try:
+
+                xplain = self._exec_dir()
+                print(xplain)
+
+            except Exception as e:
+
+                self.log.error('Execução Default sem dados de entrada. Você precisa fornecer algum script sql.', exc_info=True)
+                raise e
+
+
+        elif sql_exe_type == 'file':
+
+                xplain = self._exec_file()
+                print(xplain)
+
+        elif sql_exe_type == 'dir':
+                
+                xplain = self._exec_dir()
+                print(xplain)
